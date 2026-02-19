03d0048d9f44c524966697195e64f1579b60a531
ginatheducky <ginatheducky@gmail.com>
2026-02-19 18:47:38 +0100

    Lesson 11
    - accent color
    - gradient

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -20,6 +20,16 @@ struct PredatorDetail: View {
                     Image(predator.type.rawValue)
                         .resizable()
                         .scaledToFit()
+                        .overlay {
+                            LinearGradient(
+                                stops: [
+                                    Gradient.Stop(color: .clear, location: 0.8),
+                                    Gradient.Stop(color: .black, location: 1)
+                                ],
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        }
                     
                     // dino image
                     Image(predator.image)
@@ -79,5 +89,5 @@ struct PredatorDetail: View {
 
 #Preview {
     PredatorDetail(predator: Predators().apexPredators[2])
-        //.preferredColorScheme(.dark)
+        .preferredColorScheme(.dark)
 }
