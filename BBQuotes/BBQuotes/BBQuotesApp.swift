a5d6dcb0f74dfa684748fb6ac1c80f129c2da45d
ginatheducky <ginatheducky@gmail.com>
2026-02-24 08:51:42 +0100

    revamp of folder

---
+++ BBQuotes/BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Gina on 24.02.26.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
