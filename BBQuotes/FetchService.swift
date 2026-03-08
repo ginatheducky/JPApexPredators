e3a3cbcf529e14d735d516f239c3fe75bcb4b8fe
ginatheducky <ginatheducky@gmail.com>
2026-03-08 11:49:13 +0100

    Lesson 6
    - fetch character
    - fetch death
    - for loop

--- BBQuotes/FetchService.swift
+++ BBQuotes/FetchService.swift
@@ -36,4 +36,46 @@ struct FetchService {
         // return the quote
         return quote
     }
+    
+    func fetchCharacter(_ name: String) async throws -> Char {
+        let charURL = baseURL.appending(path: "characters")
+        let fetchURL = charURL.appending(queryItems: [URLQueryItem(name: "name", value: name)])
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let characters = try decoder.decode([Char].self, from: data)
+        
+        return characters[0]
+    }
+    
+    func fetchDeath(for character: String) async throws -> Death? {
+        let fetchURL = baseURL.appending(path: "deaths")
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let deaths = try decoder.decode([Death].self, from: data)
+        
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+        }
+        
+        return nil
+        
+    }
 }
