d29f59816b87d277895409b48991a39baa76077c
ginatheducky <ginatheducky@gmail.com>
2026-03-29 11:54:34 +0200

    Lesson 14
    - disclosure group
    -display name

--- BBQuotes/BBQuotes/Death.swift
+++ BBQuotes/BBQuotes/Death.swift
@@ -8,7 +8,7 @@ import Foundation
 
 struct Death: Decodable {
     let character: String
-    //let image: [URL] // not working for some reason
+    let image: URL // not working for some reason
     let details: String
     let lastWords: String
 }
